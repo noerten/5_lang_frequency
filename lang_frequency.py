0e0aceb6b8010935dc9dca25568b6191e5a3fd60
noerten <darkstria@gmail.com>
2016-12-24 02:23:54 +0300

    initial

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,28 @@
+from collections import Counter
+import os
+import re
 
 
-def load_data(filepath):
-    pass
+def load_text(filepath):
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath) as text_file:
+        text = text_file.read()
+    return text
 
 
-def get_most_frequent_words(text):
-    pass
+def get_word_counter(text):
+    word_dict = {}
+    words = re.findall(r'\w+', text)
+    lower_words = [word.lower() for word in words]
+    word_counter = Counter(lower_words)
+    return word_counter
+
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input('Enter file name/path: ')
+    text = load_text(filepath)
+    word_counter = get_word_counter(text)
+    top_10_words = word_counter.most_common(10)
+    print(top_10_words)
