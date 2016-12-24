198fd1cbe2a00e5182209606528fccd6f2e53fa2
noerten <darkstria@gmail.com>
2016-12-24 21:40:11 +0300

    removed magic number

--- lang_frequency.py
+++ lang_frequency.py
@@ -19,16 +19,17 @@ def get_lowered_words(text):
     return lower_words
 
 
-def get_most_frequent_words(low_words):
+def get_most_frequent_words(low_words, number_of_words):
     word_counter = Counter(low_words)
-    top_10_dict = word_counter.most_common(10)
-    top_10_list = [i[0] for i in top_10_dict]
-    return top_10_list
+    top_dict = word_counter.most_common(number_of_words)
+    top_list = [i[0] for i in top_dict]
+    return top_list
 
 
 if __name__ == '__main__':
     filepath = input('Enter file name/path: ')
+    number_of_words = int(input('Enter number of most frequent words: '))
     text = load_text(filepath)
     low_words = get_lowered_words(text)
-    top_10_words = get_most_frequent_words(low_words)
-    print(top_10_words)
+    top_n_words = get_most_frequent_words(low_words, number_of_words)
+    print(top_n_words)
