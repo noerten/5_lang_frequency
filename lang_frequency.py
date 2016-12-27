a9ebeecc55fc296d881a77f71f5bb8e0b4f8bdb1
noerten <darkstria@gmail.com>
2016-12-28 02:32:22 +0300

    refactored load function and lower func

--- lang_frequency.py
+++ lang_frequency.py
@@ -7,15 +7,11 @@ def load_text(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filepath) as text_file:
-        text = ''
-        for line in text_file:
-            text = text + line
-    return text
+        return text_file.read()
 
 
 def get_lowered_words(text):
-    words = re.findall(r'\w+', text)
-    lower_words = [word.lower() for word in words]
+    lower_words = re.findall(r'\w+', text.lower())
     return lower_words
 
 
